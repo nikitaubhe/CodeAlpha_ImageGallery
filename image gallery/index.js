886223db45a3b6473f4aa429a892c8a3526fc5be
nikitaubhe <160048490+nikitaubhe@users.noreply.github.com>
2025-01-10 19:34:33 +0530

    Add files via upload

---
+++ image gallery/index.js
@@ -0,0 +1,95 @@
+const apiKey = "U8yFUv9pz7q42x2c2Cqxa5e5Lg4ZaVg01Rvg43XrWtMFUuN6sTsUVhHx";
+const perPage = 15;
+let currentPage = 1;
+let searchTerm = null;
+
+const imagesWrapper = document.querySelector(".images");
+const loadMoreBtn = document.querySelector(".load-more");
+const searchInput = document.querySelector(".search-box input");
+const lightBox= document.querySelector(".lightbox");
+const closeBtn = lightBox.querySelector(".uil-times");
+const downloadImgBtn = lightBox.querySelector(".uil-import");
+
+const downloadImg = (imgURL) => {
+    fetch(imgURL).then(res => res.blob()).then(file => {
+        const a = document.createElement("a");
+        a.href = URL.createObjectURL(file);
+        a.download = new Date().getTime();
+        a.click();
+    }).catch(() => alert("Failed to download!"));
+}
+
+const showLightbox = (name, img) => {
+    lightBox.querySelector("img").src = img;
+    lightBox.querySelector("span").innerText = name;
+    downloadImgBtn.setAttribute("data-img",img);
+
+    lightBox.classList.add("show");
+    document.body.style.overflow = "hidden";
+
+}
+
+const hidelightBox = () => {
+    lightBox.classList.remove("show");
+    document.body.style.overflow = "auto";
+}
+
+const generateHTML = (images) => {
+    //making li of all fetched images and adding them
+    imagesWrapper.innerHTML += images.map(img =>
+        `<li class="card" onclick="showLightbox('${img.photographer}' , '${img.src.large2x}')">
+            <img src="${img.src.large2x}" alt="img">
+            <div class="details">
+                <div class="photographer">
+                    <i class="uil uil-camera">
+                        <span>${img.photographer}</span>
+                    </i>
+                </div>
+                <button onclick ="downloading('${img.src.large2x}');event.stopPropagation();">
+                 <i class="uil uil-import"></i>
+                </button>
+            </div>
+        </li>`
+    ).join("")
+}
+
+const getImages = (apiURL) => {
+    //Fetching images by API
+    loadMoreBtn.innerText = "Loading....."
+    loadMoreBtn.classList.add("disabled");
+    fetch(apiURL,{
+        headers: {Authorization:apiKey}, credentials:'same-origin'
+    }).then(res => res.json()).then(data => {
+        generateHTML(data.photos);
+        loadMoreBtn.innerText = "Load More"
+        loadMoreBtn.classList.remove("disabled");
+    }).catch(() => alert("Failed to load!!!!"));
+}
+const loadMoreImages = () => {
+    currentPage++;
+    let apiURL = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`
+    apiURL = searchTerm ? `https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`:apiURL
+    getImages(apiURL);
+}
+
+const loadSearchImages = (e) => {
+    if(e.target.value ==="") return searchTerm = null;
+    if(e.key === "Enter"){
+        currentPage = 1;
+        searchTerm = e.target.value;
+        imagesWrapper.innerHTML = "";
+        getImages(`https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`);
+      
+
+    }
+}
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
+loadMoreBtn.addEventListener("click", loadMoreImages);
+searchInput.addEventListener("keyup", loadSearchImages);
+closeBtn.addEventListener("click", hidelightBox);
+downloadImgBtn.addEventListener("click" , (e) => downloadImg(e.target.dataset.img));
+
+
+
+
